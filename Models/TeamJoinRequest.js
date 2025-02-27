1605b0a59850e3dd89c83f493fba7dad42d61aca
Yuslash <sibikrishna71@gmail.com>
2025-02-27 22:43:44 +0530

    added models for db

---
+++ Models/TeamJoinRequest.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const joinRequestSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    teamId: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true },
+    status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' },
+    createdAt: { type: Date, default: Date.now }
+})
+
+const TeamJoinRequest = mongoose.model('JoinRequest', joinRequestSchema)
+
+export default TeamJoinRequest
---
+++ Models/team.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+const teamSchema = new mongoose.Schema({
+  teamName: {
+    type: String,
+    required: true,
+    trim: true, 
+    unique: true 
+  },
+  members: [
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+      },
+      role: {
+        type: String,
+        default: 'Member', 
+        enum: ['Leader', 'Member']
+      }
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now 
+  }
+})
+
+teamSchema.pre('save', function (next) {
+  if (this.members.length > 4) 
+    return next(new Error('A team cannot have more than 4 members'))
+
+  if (this.members.length > 0 && !this.members.some(member => member.role === 'Leader'))
+    return next(new Error('A team must have a leader'))
+
+  next()
+})
+
+const Team = mongoose.model('Team', teamSchema)
+
+export default Team
